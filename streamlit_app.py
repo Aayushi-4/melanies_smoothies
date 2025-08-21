17da2272bce516d1a310ef6ed6a69e66b9032dce
Aayushi-4 <aayushi.gupta@kasmodigital.com>
2025-08-21 11:59:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
-        st.subheader(fruit_choosen + 'nutrition information')
+        st.subheader(fruit_choosen + 'Nutrition Information')
         # new section to display smoothiefruirt nutrition information
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/+ fruit_choosen")
         sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
